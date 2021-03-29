9c33c80a5a7fb55d78ddb464520d3839b5f254a1
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-03-29 01:00:03 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -59,7 +59,7 @@ module.exports = function (req, res, url) {
 					'apiserver':'/', 'isEmbed':'0', 'ctc':'go', 'tlang':'en_US', 
 					'storePath': process.env.STORE_URL + '/<store>',
 					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
-					'appCode':'go', 'siteId':'school', 'st':'', 'userId':'0DyHqK6Yj9dM', 'ut':23,'uisa':0,'theme':'family',
+					'appCode':'go', 'siteId':'school', 'st':'', 'userId':'0DyHqK6Yj9dM', 'ut':23,'uisa':0,'themeId':'family',
 					'u_info_school':'OjI6c2xoVVM3MWJIX05DMnA4cmRBQ2dFd3JvNE5xc2JEc2o4UFB2X1dVd2Eya2RPQisxVTl4d3ZHZHJPYnI4QURFNENWMjNkYm12WFdlUGxLT0g0OG4rekF5ajZhWGRGVTlocmJ4S1JhSWhCVXJlTF9BbXdyQUp3PQ=='
 
 				},
